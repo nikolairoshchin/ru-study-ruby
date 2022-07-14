61b5c8272e74da4fd97dcc26072937831054e58d
nick <nikolai.roshchin@gmail.com>
2022-07-14 12:36:27 +0300

    Added my_reduce method

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -3,37 +3,45 @@ module Exercise
     class MyArray < Array
       # Использовать стандартные функции массива для решения задач нельзя.
       # Использовать свои написанные функции для реализации следующих - можно.
-  
+
       # Написать свою функцию my_each
       def my_each(&block)
-	index = 0
-	while index < self.length
-	  block.call(self[index])
-	index += 1
-	end
-	self
+        index = 0
+        while index < length
+          block.call(self[index])
+          index += 1
+        end
+        self
       end
 
       # Написать свою функцию my_map
-      def my_map (&block)
-	result = MyArray.new
-	my_each do |el|
-	  result << block.call(el)
-	end
-	result
+      def my_map(&block)
+        result = MyArray.new
+        my_each do |el|
+          result << block.call(el)
+        end
+        result
       end
 
       # Написать свою функцию my_compact
       def my_compact
-	result = MyArray.new
-	my_each do |el|
-	  result << el unless el.nil?
-	end
-	result
+        result = MyArray.new
+        my_each do |el|
+          result << el unless el.nil?
+        end
+        result
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil, &block)
+        index = acc.nil? ? 1 : 0
+        acc ||= self[0]
+        while index < length
+          acc = block.call(acc, self[index])
+          index += 1
+        end
+        acc
+      end
     end
   end
 end
