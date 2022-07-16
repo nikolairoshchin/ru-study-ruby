1fd7f6e5380e035c04c4f005da196e3b32773745
nick <nikolai.roshchin@gmail.com>
2022-07-16 11:09:10 +0300

    Changed variables names

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,20 +2,20 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max = maximum(array)
+        my_max = maximum(array)
         new_array = []
         array.each do |arr|
-          new_array << (arr.positive? ? max : arr)
+          new_array << (arr.positive? ? my_max : arr)
         end
         new_array
       end
 
       def maximum(array)
-        max = array[0]
+        my_maxi = array[0]
         array.each do |arr|
-          max = arr if arr > max
+          my_maxi = arr if arr > my_maxi
         end
-        max
+        my_maxi
       end
 
       def search(_array, _query)
