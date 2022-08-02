ca6d00b2d20d6b134761739326ddee47f159164e
nick <nikolai.roshchin@gmail.com>
2022-08-02 09:25:37 +0300

    Changed Rating method

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,17 +4,21 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        arr = _array.map do |film|
-          film['rating_kinopoisk'].to_f if !film['country'].nil? && (film['country'].split(',').length >= 2)
+      def rating(array)
+        arr = array.map do |film|
+	  film_rating = film['rating_kinopoisk'].to_f
+          film_rating if film_2_more_country?(film) && film_rating > 0
         end.compact
-        arr.delete(0)
         arr.reduce(:+) / arr.length
       end
 
-      def chars_count(_films, _threshold)
-        arr = _films.map do |film|
-          film['rating_kinopoisk'].to_f >= _threshold ? film['name'] : 'o'
+      def film_2_more_country?(film)
+	!film['country'].nil? && film['country'].split(',').length >= 2
+      end
+
+      def chars_count(films, threshold)
+        arr = films.map do |film|
+          film['rating_kinopoisk'].to_f >= threshold ? film['name'] : 'o'
         end.map(&:chars).flatten
         arr.reduce(0) do |memo, letter|
           memo += letter == 'и' ? 1 : 0
